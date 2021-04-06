d3bd3ea2cfb698690b74553881c55b654d0ae6f3
DnxCoder <dnxcoder@gmail.com>
2021-04-05 23:18:46 -0300

    Boosted Creature animated gif

--- controllers/boostedCreature.js
+++ controllers/boostedCreature.js
@@ -5,6 +5,7 @@ const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
 const boostedCreatureURL = 'https://www.tibia.com/library/?subtopic=creatures';
+const wikiBoostedCreatureURL = 'https://tibia.fandom.com/wiki/Boosted_Creature';
 
 
 module.exports = {
@@ -27,6 +28,23 @@ module.exports = {
                 })
 
             });
+    },
+
+    getBoostedCreatureFromWikiTibia(req, res) {
+
+        got(wikiBoostedCreatureURL)
+            .then(response => {
+
+                const allDomPage = new JSDOM(response.body.toString()).window.document;
+
+                const elementContainer = allDomPage.querySelector('.compact-box');
+                const nameBoostedCreature = elementContainer.querySelector('a').text;
+                const imgLink = elementContainer.querySelector(`img[alt="${nameBoostedCreature}"]`).src;
+
+                console.log(nameBoostedCreature);
+                console.log(imgLink);
+            })
+
     }
 
 
--- routes.js
+++ routes.js
@@ -7,6 +7,8 @@ const boostedCreature = require('./controllers/boostedCreature');
 
 routes.get('/boostedCreature' , boostedCreature.getBoostedCreature);
 
+routes.get('/wikiBoostedCreature', boostedCreature.getBoostedCreatureFromWikiTibia);
+
 
 
 module.exports = routes;
