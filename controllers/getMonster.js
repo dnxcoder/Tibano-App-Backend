d66010ba13a7e59d704740f3dc2f231dd01963e3
DnxCoder <dnxcoder@gmail.com>
2021-04-13 15:07:12 -0300

    Implemented route getMonster for Test

---
+++ controllers/getMonster.js
@@ -0,0 +1,104 @@
+
+const got = require('got');
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+
+module.exports = {
+
+
+
+    getMonster: (req, res) => {
+
+
+
+        got('https://www.tibiawiki.com.br/wiki/Gigantes')
+            .then(response => {
+
+                const allDomPage = new JSDOM(response.body.toString()).window.document;
+                const tableMonsters = allDomPage.querySelector('.sortable');
+                const linesFromTableMonster = tableMonsters?.querySelectorAll('tr');
+                var charactersInfoArray
+
+
+                if (linesFromTableMonster) charactersInfoArray = Array.from(linesFromTableMonster).map((line) => {
+
+
+                    let linkImgFiltered
+                    let nameMonsterFiltered
+                    let hpFiltered
+                    let xpFiltered
+                    let charmFiltered
+
+
+                    var monsterName = line.querySelector('td:nth-child(1) a');
+                    if (monsterName) nameMonsterFiltered = monsterName?.innerHTML;
+
+                    var linkImg = line.querySelector('td:nth-child(2)');
+                    if (linkImg) linkImg = linkImg?.querySelector('img');
+                    if (linkImg) linkImgFiltered = `https://tibiawiki.com.br/${linkImg.getAttribute('src')}`;
+
+
+                    var hpColumn = line.querySelector('td:nth-child(3) ');
+                    var aFromhpColumn = line.querySelector('td:nth-child(3) a');
+
+                    if (aFromhpColumn) {
+                        if (hpColumn) {
+                            hpColumn.removeChild(aFromhpColumn);
+                            hpFiltered = hpColumn.innerHTML;
+                            hpFiltered = hpColumn.innerHTML.replace(/\n/g, '');
+                            hpFiltered = hpFiltered.replace(' ', '');
+                        }
+                    }
+
+                    var xpColumn = line.querySelector('td:nth-child(4) ');
+                    var aFromXpColumn = line.querySelector('td:nth-child(4) a');
+
+                    if (aFromXpColumn) {
+                        if (xpColumn) {
+                            xpColumn.removeChild(aFromXpColumn);
+                            xpFiltered = xpColumn.innerHTML;
+                            xpFiltered = xpFiltered.replace(/\n/g, ' ');
+                            xpFiltered = xpFiltered.replace(/ /g, '');
+                        }
+                    }
+
+                    var charmColumn = line.querySelector('td:nth-child(5)');
+                    var aCharmColum = line.querySelector('td:nth-child(5) a');
+
+                    if (aCharmColum) {
+                        if (charmColumn) {
+                            charmColumn.removeChild(aCharmColum);
+                            charmFiltered = charmColumn.innerHTML;
+                            charmFiltered = charmFiltered.replace(/\n/g, '');
+                            charmFiltered = charmFiltered.replace(/ /g, '');
+                        }
+                    }
+
+                    return (
+                        {
+                            name: nameMonsterFiltered,
+                            hp: hpFiltered,
+                            xp: xpFiltered,
+                            charm: charmFiltered,
+                            link: linkImgFiltered
+                        }
+                    )
+
+                });
+
+                charactersInfoArray.shift();
+                res.json(charactersInfoArray);
+
+
+            }).catch((error) => {
+
+                res.send('Aqui esta o erro : ' + error);
+            })
+
+
+
+    }
+
+
+}
+
--- routes.js
+++ routes.js
@@ -3,12 +3,13 @@ const routes = express.Router();
 
 
 const boostedCreature = require('./controllers/boostedCreature');
+const getMonster = require('./controllers/getMonster');
 
 
 routes.get('/boostedCreature' , boostedCreature.getBoostedCreature);
 
 routes.get('/wikiBoostedCreature', boostedCreature.getBoostedCreatureFromWikiTibia);
-
+routes.get('/getMonster', getMonster.getMonster);
 
 
 module.exports = routes;
